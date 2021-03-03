f17c05900d5c97b4e6ee69d90fc349182a227aae
Christopher Boyd <mikuana@gmail.com>
2021-03-02 22:50:16 -0800

    Fix cache unit test

--- tests/test_features.py
+++ tests/test_features.py
@@ -8,31 +8,33 @@ from data_as_code.premade import source_local
 from data_as_code._metadata import from_dictionary
 from data_as_code._step import Step, ingredient
 
+import pytest
 
-def test_cache_path_is_relative(tmpdir, default_recipe, csv_file_a):
-    """
-    Test kept file path
 
-    Ensure that the metadata artifacts path value corresponds to the expected
-    file path in the data folder, relative to the root project folder, when the
-    keep option is set to True. In other words, if we keep a file after the
-    recipe completes, the value in path should lead directly to the corresponding
-    if the working directory is set to the root project folder.
+@pytest.mark.parametrize('v_role, v_keep', [
+    ('source', True),
+    ('intermediary', True),
+    ('product', True),
+    ('product', False)  # products should be cached regardless of keep setting
+])
+def test_cache(tmpdir, default_recipe, v_role, v_keep):
     """
+    Check cache for artifacts
+    """
+    filename = 'x.csv'
     with default_recipe as r:
-        s1 = source_local(r, csv_file_a, keep=True)
-
         class Rewrite(Step):
-            """ Make Data into Code """
-            role = 'product'
-            output = Path('x.csv')
-            x = ingredient(s1)
+            output = Path(filename)
+            role = v_role
+            keep = v_keep
 
             def instructions(self):
-                self.output.write_text(self.x.path.read_text())
+                self.output.write_text('xyz')
 
         Rewrite(r)
 
-    mp = Path(r.destination, 'metadata/product/x.csv.json')
-    mj = from_dictionary(**json.loads(mp.read_text()))
-    assert mj.path.as_posix() == 'data/product/x.csv'
+    mp = Path(r.destination, 'metadata', v_role, filename + '.json')
+    assert mp.is_file()
+    mj = json.loads(mp.read_text())
+    meta = from_dictionary(**mj, relative_to=r.destination)
+    assert meta.path.is_file()
