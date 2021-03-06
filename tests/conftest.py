2cda987cc28f481d6ce129b5e4dbdb141f0c7ebd
Christopher Boyd <mikuana@gmail.com>
2021-03-05 22:44:41 -0800

    Clean up cotest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,10 @@
-import os
 import tempfile
 from pathlib import Path
 
 import pytest
-
-from data_as_code import Recipe
 
 
 @pytest.fixture
 def tmpdir():
     with tempfile.TemporaryDirectory() as d:
         yield Path(d)
-
-
-@pytest.fixture()
-def csv_file_a(tmpdir) -> Path:
-    pat = Path(tmpdir, 'fileA.csv')
-    pat.write_text('x,y,z\n1,2,3')
-    yield pat
-
-
-@pytest.fixture
-def csv_file_b(tmpdir) -> Path:
-    pat = Path(tmpdir, 'fileB.csv')
-    pat.write_text('a,b,c\n4,5,6')
-    yield pat
