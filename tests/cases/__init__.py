1b972445acd66698fd1e422c1cacdcf162b2b002
Christopher Boyd <mikuana@gmail.com>
2021-03-05 22:42:38 -0800

    Update metadata cases for tests

--- tests/cases/__init__.py
+++ tests/cases/__init__.py
@@ -7,9 +7,8 @@ def rj(name: str) -> dict:
     return json.loads(Path(Path(__file__).parent, name + '.json').read_text())
 
 
-c1 = rj('case1'), "Source without nested lineage"
-c2 = rj('case2'), "Source with single layer lineage"
-c3 = rj('case3'), "Source with single layer multi-lineage"
-c4 = rj('case4'), "Source with multi-layer lineage"
+c1 = rj('case1'), "HTTP Source"
+c2 = rj('case2'), "Local Source"
+c3 = rj('case3'), "Product with multi-step lineage"
 
-valid_cases = [c1, c2, c3, c4]
+valid_cases = [c1, c2, c3]
