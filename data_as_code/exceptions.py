d1b2695045590f21ef8f347ba353eac1f2a9ec02
Christopher Boyd <mikuana@gmail.com>
2021-05-11 22:47:27 -0700

    Remove unused exception customizations

--- data_as_code/exceptions.py
+++ data_as_code/exceptions.py
@@ -3,22 +3,11 @@ class StepError(Exception):
 
 
 class StepNoReturnAllowed(StepError):
-    def __init__(self, msg=None, *args, **kwargs):
-        default_msg = (
-            'Step instructions should not provide any return value.'
-            ' All results should be written to the Step output path.'
-        )
-        # noinspection PyArgumentList
-        super().__init__(msg or default_msg, *args, **kwargs)
+    pass
 
 
 class StepOutputMustExist(StepError):
-    def __init__(self, msg=None, *args, **kwargs):
-        default_msg = (
-            'Step instructions must populate all paths defined in output.'
-        )
-        # noinspection PyArgumentList
-        super().__init__(msg or default_msg, *args, **kwargs)
+    pass
 
 
 class StepUndefinedOutput(StepError):
